8b468dd7b5631cb860934808131968a64cd3e2dc
Inferno4you <xavierdillinger@gmail.com>
2019-08-02 01:44:48 -0400

    Stars

--- src/main/java/client/renderables/Background.java
+++ src/main/java/client/renderables/Background.java
@@ -9,11 +9,16 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Background implements Entity, Drawable {
     private BufferedImage texture;
     private Color c = Color.BLACK;
     private World world;
+    private Random r = new Random();
+    private ArrayList<Point> points = new ArrayList<>();
+
 
     public Background(World world) {
         this.world = world;
@@ -36,10 +41,17 @@ public class Background implements Entity, Drawable {
 
     @Override
     public void second() {
-        if (world.getTime() >= 120) {
+        points.clear();
+        if (world.getTime() >= 1200) {
             c = c.brighter();
-        } else if ( world.getTime() <= 130){
+        } else if ( world.getTime() <= 1201){
             c = c.darker();
+        }
+        for (int i = 0; i < 20; i++) {
+            int x = r.nextInt(Game.WIDTH-1);
+            int y = r.nextInt(Game.HEIGHT/2-1);
+            Point p = new Point(x,y);
+            points.add(p);
         }
     }
 
@@ -48,5 +60,9 @@ public class Background implements Entity, Drawable {
         g.setColor(c);
         g.fillRect(0,0, Game.WIDTH, Game.HEIGHT);
         g.drawImage(texture, 0,0, Game.WIDTH, Game.HEIGHT, c, null);
+        g.setColor(Color.WHITE);
+        for (Point p : points) {
+            g.drawRect(((int) p.getX()), (int) p.getY(), 1, 1);
+        }
     }
 }
