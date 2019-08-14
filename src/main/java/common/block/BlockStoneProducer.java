a42b134b7cef383ce5487bc793f9cb5bb383a9e8
Inferno4you <xavierdillinger@gmail.com>
2019-08-14 03:19:10 -0400

    Changed some recipes for stone produce.

--- src/main/java/common/block/BlockStoneProducer.java
+++ src/main/java/common/block/BlockStoneProducer.java
@@ -33,7 +33,22 @@ public class BlockStoneProducer extends Block {
         } else if (Blocks.log.getName().equals(Game.headsUpDisplay.getSelected().getItemStack().getItem().getName())&& Game.headsUpDisplay.getSelected().getItemStack().getAmount() >= 8){
             for (int i = 0; i < Game.headsUpDisplay.getSlots().length; i++) {
                 if (Game.headsUpDisplay.getSlots()[i].getItemStack().getItem() instanceof ItemBlock) {
-                    if (((ItemBlock) Game.headsUpDisplay.getSlots()[i].getItemStack().getItem()).getBlock() == Blocks.sapling) {
+                    if (((ItemBlock) Game.headsUpDisplay.getSlots()[i].getItemStack().getItem()).getBlock() == Blocks.stone) {
+                        Game.headsUpDisplay.getSlots()[i].getItemStack().setAmount(Game.headsUpDisplay.getSlots()[i].getItemStack().getAmount() + 1);
+                        Game.headsUpDisplay.getSelected().getItemStack().setAmount(Game.headsUpDisplay.getSelected().getItemStack().getAmount()-8);
+                        break;
+                    } else if (((ItemBlock) Game.headsUpDisplay.getSlots()[i].getItemStack().getItem()).getBlock().isAir()){
+                        Game.headsUpDisplay.getSlots()[i].getItemStack().setItem(Items.getItem(Blocks.stone.getName()));
+                        Game.headsUpDisplay.getSlots()[i].getItemStack().setAmount(1);
+                        Game.headsUpDisplay.getSelected().getItemStack().setAmount(Game.headsUpDisplay.getSelected().getItemStack().getAmount()-8);
+                        break;
+                    }
+                }
+            }
+        }else if (Blocks.planks.getName().equals(Game.headsUpDisplay.getSelected().getItemStack().getItem().getName())&& Game.headsUpDisplay.getSelected().getItemStack().getAmount() >= 4){
+            for (int i = 0; i < Game.headsUpDisplay.getSlots().length; i++) {
+                if (Game.headsUpDisplay.getSlots()[i].getItemStack().getItem() instanceof ItemBlock) {
+                    if (((ItemBlock) Game.headsUpDisplay.getSlots()[i].getItemStack().getItem()).getBlock() == Blocks.stone) {
                         Game.headsUpDisplay.getSlots()[i].getItemStack().setAmount(Game.headsUpDisplay.getSlots()[i].getItemStack().getAmount() + 1);
                         Game.headsUpDisplay.getSelected().getItemStack().setAmount(Game.headsUpDisplay.getSelected().getItemStack().getAmount()-4);
                         break;
