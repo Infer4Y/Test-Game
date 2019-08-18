74caa23a6f66a38f8c5bc62f0e776503eb1724f9
XavierDD1st <xavierdd1st@textnow.me>
2019-08-17 22:40:15 -0400

    Crafting is broken atm.

--- src/main/java/client/renderables/CraftingUI.java
+++ src/main/java/client/renderables/CraftingUI.java
@@ -1,10 +1,13 @@
 package client.renderables;
 
+import common.item.ItemStack;
 import common.recipes.Recipe;
+import common.registries.Items;
 import common.registries.Recipes;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 
 public class CraftingUI {
     private CraftingSlots[][] craftingSlots = new CraftingSlots[5][3];
@@ -21,7 +24,7 @@ public class CraftingUI {
         }
         for (int i = 0; i < recipesFromCraftingSlots.length; i++) {
             for (int j = 0; j < recipesFromCraftingSlots[i].length; j++) {
-                recipesFromCraftingSlots[i][j] = new Recipe(null, null, null);
+                recipesFromCraftingSlots[i][j] = new Recipe("null", Arrays.asList(new ItemStack[]{new ItemStack(Items.getItem("air"), 0)}), Arrays.asList(new ItemStack[]{new ItemStack(Items.getItem("air"), 0)}));
             }
         }
         craftingSlots[0][0].setItemStack(Recipes.planks.getResult().get(0));
@@ -30,6 +33,12 @@ public class CraftingUI {
         recipesFromCraftingSlots[0][1] = Recipes.sticks;
         craftingSlots[0][2].setItemStack(Recipes.stone_producer.getResult().get(0));
         recipesFromCraftingSlots[0][1] = Recipes.sticks;
+        ingredientSlots = new CraftingSlots[recipesFromCraftingSlots[0][0].getIngredients().size()];
+        for (int i = 0; i < ingredientSlots.length; i++) {
+            ingredientSlots[i] = new CraftingSlots();
+            ingredientSlots[i].setItemStack(recipesFromCraftingSlots[0][0].getIngredients().get(i));
+            ingredientSlots[i].setRectangle(new Rectangle(4 * 36 + 4, i * 36 + 64, 36, 36));
+        }
     }
 
     public void draw(Graphics2D g){
@@ -42,27 +51,81 @@ public class CraftingUI {
                 }
             }
         }
+        for (int i = 0; i < ingredientSlots.length; i++) {
+            try {
+                ingredientSlots[i].draw(g, ingredientSlots[i].getRectangle().x,ingredientSlots[i].getRectangle().y, false);
+            } catch (NullPointerException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void handleMouseMove(MouseEvent e){
         loc = e.getPoint().getLocation();
+        for (int i = 0; i < craftingSlots.length; i++) {
+            for (int j = 0; j < craftingSlots[i].length; j++) {
+                if (craftingSlots[i][j].getRectangle().contains(loc)){
+                    if ((!recipesFromCraftingSlots[i][j].getName().equals("null"))) {
+                        try {
+                            ingredientSlots = new CraftingSlots[recipesFromCraftingSlots[i][j].getIngredients().size() + 1];
+                            for (int l = 0; i < ingredientSlots.length; l++) {
+                                ingredientSlots[l] = new CraftingSlots();
+                                ingredientSlots[l].setItemStack(recipesFromCraftingSlots[i][j].getIngredients().get(l));
+                                ingredientSlots[l].setRectangle(new Rectangle(4 * 36 + 4, i * 36 + 64, 36, 36));
+                            }
+                        } catch (NullPointerException l){
+                            l.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void handleMouseClick(MouseEvent e){
         loc = e.getPoint().getLocation();
-        if (e.getButton() == MouseEvent.BUTTON1) {
+        if (e.getButton() == MouseEvent.BUTTON1 && !e.isShiftDown()) {
             for (int i = 0; i < craftingSlots.length; i++) {
                 for (int j = 0; j < craftingSlots[i].length; j++) {
                     if (craftingSlots[i][j].getRectangle().contains(loc)){
-                        if (recipesFromCraftingSlots[i][j] != null) {
+                        if ((!recipesFromCraftingSlots[i][j].getName().equals("null"))) {
                             if (recipesFromCraftingSlots[i][j].isCraftable()) {
                                 recipesFromCraftingSlots[i][j].craft();
+                            }
+                            try {
+                                ingredientSlots = new CraftingSlots[recipesFromCraftingSlots[i][j].getIngredients().size() + 1];
+                                for (int l = 0; i < ingredientSlots.length; l++) {
+                                    ingredientSlots[l] = new CraftingSlots();
+                                    ingredientSlots[l].setItemStack(recipesFromCraftingSlots[i][j].getIngredients().get(l));
+                                    ingredientSlots[l].setRectangle(new Rectangle(4 * 36 + 4, i * 36 + 64, 36, 36));
+                                }
+                            } catch (NullPointerException l){
+                                l.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < craftingSlots.length; i++) {
+                for (int j = 0; j < craftingSlots[i].length; j++) {
+                    if (craftingSlots[i][j].getRectangle().contains(loc)){
+                        if (!recipesFromCraftingSlots[i][j].getName().equals("null")) {
+                            try {
+                                ingredientSlots = new CraftingSlots[recipesFromCraftingSlots[i][j].getIngredients().size() + 1];
+                                for (int l = 0; i < ingredientSlots.length; l++) {
+                                    ingredientSlots[l] = new CraftingSlots();
+                                    ingredientSlots[l].setItemStack(recipesFromCraftingSlots[i][j].getIngredients().get(l));
+                                    ingredientSlots[l].setRectangle(new Rectangle(4 * 36 + 4, i * 36 + 64, 36, 36));
+                                }
+                            } catch (NullPointerException l){
+                                l.printStackTrace();
                             }
                         }
                     }
                 }
             }
         }
+
     }
 
     public void open(){}
