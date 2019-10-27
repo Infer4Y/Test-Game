6b9431deb6e7ff04c4de937279edee827563853e
Inferno4you <xavierdillinger@gmail.com>
2019-10-27 10:41:36 -0400

    Fixed the gradlew.

--- src/main/kotlin/client/ClientGame.kt
+++ src/main/kotlin/client/ClientGame.kt
@@ -1,5 +1,6 @@
 package client
 
+import client.renderer.Window
 import common.Game
 
 
@@ -9,11 +10,18 @@ class ClientGame : Game() {
     val WIDTH = 1280
     val HEIGHT = 640
 
+    init {
+        window = Window(NAME, WIDTH, HEIGHT)
+        window.run()
+    }
+
     override fun update() {
         world.update()
     }
 
     companion object {
+        @JvmStatic
+        lateinit var window: Window
         @JvmStatic
         lateinit var instance: ClientGame
     }
--- src/main/kotlin/client/renderer/Window.kt
+++ src/main/kotlin/client/renderer/Window.kt
@@ -1,14 +1,100 @@
 package client.renderer
 
-import java.awt.Dimension
-import javax.swing.JFrame
+import org.lwjgl.glfw.Callbacks.glfwFreeCallbacks
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.system.MemoryStack.stackPush
+import org.lwjgl.system.MemoryUtil.NULL
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
 
-class Window (name: String?, sizeX: Int, sizeY : Int) : JFrame(name) {
-    init {
-        val dim = Dimension(sizeX, sizeY)
-        this.maximumSize = dim
-        this.preferredSize = dim
-        this.minimumSize = dim
-        this.size = dim
+
+
+
+class Window (var name: String?, var sizeX: Int, var sizeY : Int){
+    private var window: Long = 0
+
+    fun run(){
+        init()
+        loop()
+
+        // Free the window callbacks and destroy the window
+        glfwFreeCallbacks(window)
+        glfwDestroyWindow(window)
+
+        // Terminate GLFW and free the error callback
+        glfwTerminate()
+        glfwSetErrorCallback(null)?.free()
+    }
+
+    fun init(){
+        // Setup an error callback. The default implementation
+        // will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set()
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        check(glfwInit()) { "Unable to initialize GLFW" }
+
+        // Configure GLFW
+        glfwDefaultWindowHints() // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE) // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE) // the window will be resizable
+
+        // Create the window
+        window = glfwCreateWindow(sizeX, sizeY, name, NULL, NULL)
+        if (window === NULL)
+            throw RuntimeException("Failed to create the GLFW window")
+
+        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
+        glfwSetKeyCallback(window) { window, key, scancode, action, mods ->
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true) // We will detect this in the rendering loop
+        }
+
+        // Get the thread stack and push a new frame
+        stackPush().use { stack ->
+            val pWidth = stack.mallocInt(1) // int*
+            val pHeight = stack.mallocInt(1) // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight)
+
+            // Get the resolution of the primary monitor
+            val vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor())
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode!!.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            )
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window)
+        // Enable v-sync
+        glfwSwapInterval(1)
+
+        // Make the window visible
+        glfwShowWindow(window)
+    }
+
+    fun loop(){
+        GL.createCapabilities()
+
+        // Set the clear color
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f)
+
+        // Run the rendering loop until the user has attempted to close
+        // the window or has pressed the ESCAPE key.
+        while (!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT) // clear the framebuffer
+
+            glfwSwapBuffers(window) // swap the color buffers
+
+            // Poll for window events. The key callback above will only be
+            // invoked during this call.
+            glfwPollEvents()
+        }
     }
 }
