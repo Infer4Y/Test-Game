354100d4ce2b81375fdaa0bccd7f1bbf99130583
inferno4you <46350196+inferno4you@users.noreply.github.com>
2020-05-14 03:38:45 -0400

    Changing the amount of entities.

--- src/test/java/inferno/common/world/WorldTest.java
+++ src/test/java/inferno/common/world/WorldTest.java
@@ -27,9 +27,9 @@ public class WorldTest {
         testingPlayer2.setLocation(new Vector2f(0,100));
         Player testingPlayer3 = new Player("testplayer3",1,1);
         testingPlayer3.setLocation(new Vector2f(0,-100));
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             Entity temp = new Entity("test"+i,1,1);
-            temp.setLocation(new Vector2f(Referance.RANDOM.nextInt(200) - 100, Referance.RANDOM.nextInt(200) - 100));
+            temp.setLocation(new Vector2f(Referance.RANDOM.nextInt(400) - 200, Referance.RANDOM.nextInt(400) - 200));
             testingWorld.addEntity(temp);
         }
 
